501d6d7519b146b34908322fd71585d587a25e75
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:32 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,22 +3,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "ee.forgr.plugin.bluetoothlowenergy.example",
-  "appName": "BLE Example",
-  "webDir": "dist",
-  "server": {
-    "androidScheme": "https"
+  appId: 'ee.forgr.plugin.bluetoothlowenergy.example',
+  appName: 'BLE Example',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
   },
-  "plugins": {
-    "CapacitorUpdater": {
-      "appId": "ee.forgr.plugin.bluetoothlowenergy.example",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+  plugins: {
+    CapacitorUpdater: {
+      appId: 'ee.forgr.plugin.bluetoothlowenergy.example',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
